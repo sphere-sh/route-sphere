d6cb849ba36f9ec324ccf03571818a134bb85801
Romano Schoonheim <stal@romanoschoonheim.nl>
2025-03-03 12:11:28 +0100

    Define docker-compose.yaml

---
+++ .docker/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker build -t bromanonld/route-sphere:latest -f .docker/Dockerfile .
+docker push bromanonld/route-sphere:latest
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module route-sphere
 
 go 1.23
+
+require gopkg.in/yaml.v3 v3.0.1 // indirect
