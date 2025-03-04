37037a4c7425c01346adfb5dca59e87ca9ab97e1
Romano Schoonheim <stal@romanoschoonheim.nl>
2025-03-04 12:56:15 +0100

    Specify README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+# Orrery - Route Sphere
+
+Route Sphere is a high-performance reverse proxy designed to intelligently manage and distribute incoming traffic across
+multiple services based on rules.
--- domain.go
+++ domain.go
@@ -2,13 +2,5 @@ package route_sphere
 
 type Domain struct {
 	// URL - domain URL
-	URL string `yaml:"url"`
-
-	// TLS - TLS Configuration
-	TLS struct {
-		// Cert - path to certificate file
-		Cert string `yaml:"cert"`
-		// Key - path to key file
-		Key string `yaml:"key"`
-	}
+	Url string `yaml:"url"`
 }
